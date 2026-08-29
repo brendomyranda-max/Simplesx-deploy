a4fd0e6a9a5215cde5ce0d7c4144700a71f1df57
brendomyranda-max <brendo.myranda@gmail.com>
2026-08-28 23:23:03 -0300

    Prepara gestores 1.5.6

--- gestor-android/app/build.gradle.kts
+++ gestor-android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.simplesx.gestor"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "1.5.5"
+        versionCode = 19
+        versionName = "1.5.6"
     }
 
     buildFeatures { compose = true }
